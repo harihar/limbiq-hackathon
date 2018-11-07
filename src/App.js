c39eec0b3c8355e76c16600c4069607a500fbe0b
Dami <damian.garciagarcia@gmail.com>
2018-11-07 16:18:03 +0100

    Add navigation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import InputAdornment from "@material-ui/core/InputAdornment";
 import SearchRounded from "@material-ui/icons/SearchRounded";
 
 import OrdersList from "./components/orders-list/OrdersList";
+import Navigation from "./components/navigation/Navigation";
 
 import "./App.css";
 
@@ -12,6 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <Navigation />
         <div style={{ float: "right" }}>
           <TextField
             style={{ margin: "normal", marginRight: "100px" }}
