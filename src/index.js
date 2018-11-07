3491ab4cad1c6c65ad378f89c36306868b892053
Florian Schmidt <florian.schmidt4@volkswagen.de>
2018-11-07 16:21:52 +0100

    Add very very basic routing for the showcase

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import DashboardAli from "./components/dashboard-ali/DashboardAli";
+import DashBoard from "./components/DashBoard";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (window.location.href.endsWith("dashboardAli")) {
+    ReactDOM.render(<DashboardAli/>, document.getElementById('root'));
+} else if (window.location.href.endsWith("orders")) {
+    ReactDOM.render(<App/>, document.getElementById('root'));
+} else {
+    ReactDOM.render(<DashBoard/>, document.getElementById('root'));
+}
