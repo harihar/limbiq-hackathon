357d562e83dd6b7c10eeb18a81c3a38e9ba476b5
Florian Schmidt <florian.schmidt4@volkswagen.de>
2018-11-07 16:08:08 +0100

    Restore default behaviour to load App.js at the beginning

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import DashboardAli from './DashboardAli';
+import App from './App';
 
-ReactDOM.render(<DashboardAli />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
